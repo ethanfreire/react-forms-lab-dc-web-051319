7604deb9e79ddee1a53c2d6fc75a0be8858b961c
Your Name <you@example.com>
2019-07-17 22:26:12 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,33 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ''
+    };
   }
+  onChangeEventHandler = (event) => {
 
+  this.setState({
+    value: event.target.value,
+  });
+}
+  valueRemainingChar = () => {
+     if((this.props.maxChars - this.state.value.length) < 0){
+
+      alert("limit char reached") 
+    }else {
+      return (this.props.maxChars - this.state.value.length)
+    }
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message"
+        value = {this.state.value}
+        onChange = {this.onChangeEventHandler}
+        />
+        Remaining Characters: {this.valueRemainingChar()}
       </div>
     );
   }
